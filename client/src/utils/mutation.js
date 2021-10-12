a34675463468637f11c51b9974aa861b113aa943
katerz avocado <ktrinaapodaca@yahoo.com>
2021-10-11 18:39:13 -0600

    updating new rep since old one was not working

---
+++ client/src/utils/mutation.js
@@ -0,0 +1,62 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+// $ variable and data type after
+// ask for assistance
+export const SAVED_BOOK = gql`
+  mutation saveBooks($input: saveBook) {
+    saveBooks(input: $input) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation deleteBook($bookID: ID!) {
+    deleteBook(bookID: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
